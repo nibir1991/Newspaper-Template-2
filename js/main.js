779e4a97895e9fec689f91c64b673047fae96b6b
Nibir <nibir.rayhan1991@gmail.com>
2020-11-16 21:27:12 +0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper('.swiper-container2', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 2,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
+
+
